5a87fd117e0f9b6a32a017febae9497d9c1715f1
LemonCat1019 <115674619+LemonCat1019@users.noreply.github.com>
2022-11-09 21:16:57 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.lightLevel())
 })
 input.onButtonPressed(Button.B, function () {
-    led.setBrightness(input.lightLevel())
-    basic.showNumber(input.lightLevel())
+    if (true) {
+        led.setBrightness(input.lightLevel())
+        basic.showNumber(input.lightLevel())
+    }
 })
 basic.forever(function () {
 	
